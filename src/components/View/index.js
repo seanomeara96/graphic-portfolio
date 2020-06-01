42efe1528b16da33ba3c6e102288fbbca9c91951
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2020-06-01 21:50:09 +0100

    commented

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Return from "../Return";
-
+/*
+* Those divs could probably be moved to the Return component.
+* Image should be passed via props from App.
+* Tap to see the "after" picture feature needs to be implemented.
+* A simple juxtaposition is just fine for larger screens. 
+*/
 // View component
 export default props => {
     console.log(props)

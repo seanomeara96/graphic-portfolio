616c9f4eb3b9d34039b550d2a654b806dee92ec2
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2020-06-01 21:37:46 +0100

    commented

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,5 @@
+// Probably just remove this altogether or add some external links 
+// Whatever is less of an eye-sore
 import React from "react";
 import styles from "./Footer.module.css";
 export default () => {

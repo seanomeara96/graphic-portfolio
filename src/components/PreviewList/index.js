5f83f64dfcfb71b5d84749fbfe88ccc0d28b7081
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2020-06-01 21:44:00 +0100

    commented

--- src/components/PreviewList/index.js
+++ src/components/PreviewList/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./PreviewList.module.css";
 import Preview from "../Preview";
-
+/*
+* Needs a for loop.
+* Image URLS should be passed from main App component.
+*/
 // Preview-List component
 export default props => {
     return (

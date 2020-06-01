fe1d8252dfcbbfc60fa851f98829c68cf425b232
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2020-06-01 21:35:30 +0100

    commented

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,11 @@ import Header from "../Header";
 import Footer from "../Footer";
 import PreviewList from "../PreviewList";
 import View from "../View";
-
+/*
+* Can URL prams be passed via props without redux?
+* Need to change router type to be able to history.push("/")
+  rather than just linking to the home screen.
+*/
 // App component
 export default () => {
     return (

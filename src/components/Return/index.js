3440a9949e4206f10a90c123c08d893bf52eed84
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2020-06-01 21:45:36 +0100

    commented

--- src/components/Return/index.js
+++ src/components/Return/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Return.module.css";
 
+// Learn how to design buttons. This sucks.
+
 // Return Button
 export default () => {
     return <Link className={styles.button} to="/" >Go Back!</Link>

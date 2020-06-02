b7deabfa716c0f9bf838c60de4cbb02f877a29c7
seanomeara96 <seanom96@gmail.com>
2020-06-02 11:06:28 +0100

    refactored

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./Preview.module.css";
+import {container, card, back, image, front} from "./Preview.module.css";
 
 // Preview component
 export default ({before, after}) => {
     return (
-        <Link to="/other" className={styles.container}>
+        <Link to="/other" className={container}>
             <div 
-            className={`${styles.card} ${styles.back}`} >
-                <img className={styles.image} src={after} alt="photoshop"></img>
+            className={`${card} ${back}`} >
+                <img className={image} src={after} alt="photoshop"></img>
             </div>
-            <div className={`${styles.card} ${styles.front}`} >
-                <img className={styles.image} src={before} alt="photoshop"></img>
+            <div className={`${card} ${front}`} >
+                <img className={image} src={before} alt="photoshop"></img>
             </div>
         </Link>
     );

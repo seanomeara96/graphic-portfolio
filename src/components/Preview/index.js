ba693c87ad17291886c4c55cdfe4e5e3af081efa
seanomeara96 <seanom96@gmail.com>
2020-06-02 11:13:54 +0100

    key is not a prop

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import {container, card, back, image, front} from "./Preview.module.css";
 
 // Preview component
-export default ({before, after, key}) => {
+export default ({before, after}) => {
     return (
         <Link to="/other" className={container}>
             <div 
             className={`${card} ${back}`} >
-                <img className={image} src={after} alt={`photoshop ${key}`}></img>
+                <img className={image} src={after} alt={`photoshop`}></img>
             </div>
             <div className={`${card} ${front}`} >
-                <img className={image} src={before} alt={`photoshop ${key}`}></img>
+                <img className={image} src={before} alt={`photoshop`}></img>
             </div>
         </Link>
     );

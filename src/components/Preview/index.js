7c8255d037b226634ad9f73dc0378b87d21bea68
seanomeara96 <seanom96@gmail.com>
2020-06-02 11:04:01 +0100

    refactored

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import styles from "./Preview.module.css";
-/*
-* Component needs images.
-* Refactor for string interpolation.
-*/
+
 // Preview component
 export default ({before, after}) => {
     return (
         <Link to="/other" className={styles.container}>
-            <div className={styles.card  + " " +  styles.back} >
+            <div 
+            className={`${styles.card} ${styles.back}`} >
                 <img className={styles.image} src={after} alt="photoshop"></img>
             </div>
-            <div className={styles.card + " " + styles.front} >
+            <div className={`${styles.card} ${styles.front}`} >
                 <img className={styles.image} src={before} alt="photoshop"></img>
             </div>
         </Link>

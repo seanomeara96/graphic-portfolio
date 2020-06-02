c4101aec55a7d1e21ac2015937a7b3c3a677712a
seanomeara96 <seanom96@gmail.com>
2020-06-02 11:35:31 +0100

    Updates

---
+++ precache-manifest.a7aab39cfd83b24730bcd566ddc29bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9a2abd3ff58e21a68c90538eb870c3",
+    "url": "/graphic-portfolio/index.html"
+  },
+  {
+    "revision": "abf109971f2433c06825",
+    "url": "/graphic-portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e8d0a95eac053ed7a7c5",
+    "url": "/graphic-portfolio/static/css/main.f08a436b.chunk.css"
+  },
+  {
+    "revision": "abf109971f2433c06825",
+    "url": "/graphic-portfolio/static/js/2.01243594.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/graphic-portfolio/static/js/2.01243594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d0a95eac053ed7a7c5",
+    "url": "/graphic-portfolio/static/js/main.6b7958cc.chunk.js"
+  },
+  {
+    "revision": "4f9573024e2b57201100",
+    "url": "/graphic-portfolio/static/js/runtime-main.5e8913fb.js"
+  }
+]);
